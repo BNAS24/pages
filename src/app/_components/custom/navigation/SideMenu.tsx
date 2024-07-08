c6c69b332cedbe6b627c9a26a99fe9795c03f460
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2024-07-08 00:31:19 -0400

    side menu navigation enhanced and bug fixes

--- src/app/_components/custom/navigation/SideMenu.tsx
+++ src/app/_components/custom/navigation/SideMenu.tsx
@@ -2,13 +2,14 @@ import { Container } from "@mui/material";
 import { animated, useSpring } from "@react-spring/web";
 import { useState } from "react";
 import { useMenu } from "@/app/_hooks/useBodyStyle";
+import { theme } from "@/app/_styles/muiTheme";
 
 const SideMenuContainer = animated(Container);
 
 export const SideMenu = () => {
   const [animationComplete, setAnimationComplete] = useState(true);
 
-  const { menuOpen } = useMenu();
+  const { menuOpen, handleMenuOpen } = useMenu();
 
   // Define spring animation configuration
   const spring = useSpring({
@@ -25,20 +26,33 @@ export const SideMenu = () => {
 
   return (
     <SideMenuContainer
-    disableGutters={true}
-    maxWidth={false}
+      disableGutters={true}
+      maxWidth={false}
       style={spring}
+      onClick={handleMenuOpen}
       sx={{
         position: "fixed",
         display: !animationComplete ? "flex" : "none",
         top: "64px",
         right: 0,
         height: "100%",
-        backgroundColor: "grey",
+        backgroundColor: "rgba(0, 0, 0, 0.40)",
+        backdropFilter: "blur(8px) ",
         zIndex: 101,
       }}
     >
-      This a side nav
+      <Container
+        sx={{
+          position: "absolute",
+          right: 0,
+          width: "80%",
+          height: "100%",
+          backgroundColor: theme.palette.primary.main,
+        }}
+      >
+        {" "}
+        This a side nav
+      </Container>
     </SideMenuContainer>
   );
 };
