467af2e913d97746e71d718b4a5616f0dc487398
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2024-07-08 23:02:54 -0400

    ...

--- src/app/_types/types/menuContextType.ts
+++ src/app/_types/types/menuContextType.ts
@@ -1,3 +1,5 @@
+import { NavigationState } from "@/app/_types/interfaces/navigationState";
+
 export type MenuContextType = {
   bodyStyles: NavigationState;
   setBodyStyles: (state: NavigationState) => void;
--- src/app/categories/layout.tsx
+++ src/app/categories/layout.tsx
@@ -1,9 +1,18 @@
 import { Container } from "@mui/material";
 import { LayoutProps } from "@/app/_types/interfaces/layoutProps";
+// import { theme } from "@/app/_styles/muiTheme";
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <Container disableGutters={true} maxWidth={false}>
+    <Container
+      disableGutters={true}
+      maxWidth={false}
+      sx={{
+        width: '100%',
+        minHeight: "100%",
+        // backgroundColor: "#DFE2F1",
+      }}
+    >
       {children}
     </Container>
   );
