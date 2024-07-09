ed15bb1de8b35e4ce1ab5ec5fcd47eb53efd8d64
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2024-07-08 22:49:36 -0400

    ...

--- src/app/_hooks/useBodyStyle.tsx
+++ src/app/_hooks/useBodyStyle.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { NavigationState } from "@/app/_types/interfaces/navigationState";
-import { MenuContextType } from "@/app/_types/types/navigationState";
+import { MenuContextType } from "@/app/_types/types/menuContextType";
 
 const MenuContext = createContext<MenuContextType>({} as MenuContextType);
 
