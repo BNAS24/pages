978ebdfd6724ab9b6f9be39ba7682467ac03ed92
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2024-08-06 13:31:06 -0400

    images optimization removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        // Removes limit of 1000 images being optimized by vercel
+        unoptimized: true,
         domains: ['books.google.com'],
     },
 };
